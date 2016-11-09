5098352219cf6da05bc0e34ef0a231d204121a54
domicb <domi1213@hotmail.com>
2016-11-09 23:46:16 +0100

    TERMINADO LANDING
    terminamos la portada

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class landing extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -21,6 +21,6 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
              $this->load->helper('url');
-		$this->load->view('landing.html');
+		$this->load->view('index.html');
 	}
 }
