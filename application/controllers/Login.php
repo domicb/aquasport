83f8444114b95a80e9f08aba5bfe6e3d68855d54
domicb <domi1213@hotmail.com>
2016-11-10 22:38:49 +0100

    metemos archivos para login mcv

---
+++ application/controllers/Login.php
@@ -0,0 +1,44 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+	$this->load->model('Usuarios_model');
+    }
+	
+	public function index()
+        {	           
+            if(isset($_POST['username']) && isset($_POST['password']))
+            {
+                $pass = $_POST['password'];
+                $ema = $_POST['username'];
+                //si la sesion está creada redirigimos a inicio
+                if($this->session->userdata('username'))
+                {
+                    redirect(base_url());
+                }
+                //si devuelve 1 es true sino false
+                 if($this->Usuarios_model->ValidarUsuario($ema,$pass))
+                 {//si no estaba logeado pero si esta en la base de datos le damos acceso con set_userdata
+                     $this->session->set_userdata('username',$ema);
+                     redirect(base_url());
+                 }
+                 else
+                 {//si no tiene cuenta lo mandamos de nuevo al login
+                     redirect(base_url().'index.php/Login/');                  
+                 }
+            }
+            
+            $carro = $this->load->view('Vista_login','',true);
+            $this->load->view('Plantilla_carro',Array('carro'=>$carro));
+        }
+        public function logout()
+        {
+            $this->session->sess_destroy();
+            redirect(base_url());
+        }
+}
+
---
+++ application/views/Vista_login.php
@@ -0,0 +1,42 @@
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login</title>
+
+     <link href="<?=base_url()?>Assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?=base_url()?>Assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>Assets/css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-md-12">
+            <div class="col-md-4"></div>
+            <div class="col-md-4" id="login">
+                <form class="form-signin" role="form" action="<?=base_url()?>index.php/Login/" method="POST">
+                    <div class="text-center">
+                        <img id="avatar" src="<?=base_url()?>Assets/img/nadie.png" alt="avatar">
+                    </div>
+                    <input id="txtEmail" type="email" name="username" class="form-control" placeholder="Email">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesión</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div id="nebaris">
+        <a href="<?=base_url().'index.php/Envio_email'?>">¿No tienes cuenta?</a>   
+        <a href="<?=base_url().'index.php/Usuarios_ci/contra'?>">Restablecer contraseña</a>           
+        <a href="<?=base_url()?>">Volver a inicio</a>   
+    </div>
+    <script src="<?=base_url()?>Assets/js/jquery.js"></script>
+    <script src="<?=base_url()?>Assets/js/jquery.md5.min.js"></script>
+     <script src="<?=base_url()?>Assets/js/bootstrap.min.js"></script>
+    <script src="<?=base_url()?>Assets/js/script.js"></script>
+</body>
+
+</html>
