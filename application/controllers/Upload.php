e1a836f1f723a12d2325a085898756232c5791e9
domicb <domi1213@hotmail.com>
2016-11-13 23:29:44 +0100

    cambiamos el maximo del post

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -17,7 +17,7 @@ class Upload extends CI_Controller {
     //FUNCIÓN PARA SUBIR LA IMAGEN Y VALIDAR EL TÍTULO
     function do_upload() {
         $this->form_validation->set_rules('titulo', 'titulo', 'required|min_length[5]|max_length[20]|trim|xss_clean');
-        $this->form_validation->set_rules('cuerpo', 'cuerpo', 'required|min_length[35]|max_length[5800]');
+        $this->form_validation->set_rules('cuerpo', 'cuerpo', 'required|min_length[35]|max_length[8000]');
         $this->form_validation->set_rules('autor', 'autor', 'required|min_length[5]|max_length[20]|trim|xss_clean');
         $this->form_validation->set_message('required', 'El %s no puede ir vacío!');
         $this->form_validation->set_message('min_length', 'El %s debe tener al menos %s carácteres');
