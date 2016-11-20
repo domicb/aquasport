79823fbb1d593619154f9214525c810995ca1161
domicb <domi1213@hotmail.com>
2016-11-20 03:39:34 +0100

    quitamos el li que sobra del carrusel

--- application/views/Cuerpo.php
+++ application/views/Cuerpo.php
@@ -6,7 +6,6 @@
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                         </ol>
                         <div class="carousel-inner">
                             <div class="item active">
