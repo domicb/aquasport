b26e4d70831a094afc72ee1d62ca8f4b25450840
domicb <domi1213@hotmail.com>
2016-12-01 18:06:01 +0100

    separamos codigo js de html
    para ello hemos metido el js en el archivo funciones.js

---
+++ assets/js/funciones.js
@@ -0,0 +1,45 @@
+
+            $(document).ready(function () {
+                $("#temp").toggle();
+                $("#top").click(function () {
+                    var posicion = $("#aqui").offset().top;
+                    $("html, body").animate({scrollTop: posicion});
+                });
+                $("#btngaleria").click(function () {
+                    var pos = $("#galeria").offset().top;
+                    $("html, body").animate({scrollTop: pos});
+                });
+                $("#btnsecciones").click(function () {
+                    var posi = $("#secciones").offset().top;
+                    $("html, body").animate({scrollTop: posi});
+                });
+                $("#btneltiempo").click(function () {
+                    var posi = $("#eltiempo").offset().top;
+                    $("html, body").animate({scrollTop: posi});
+                });
+                $(".tiem").click(function () {
+                    var posi = $("#eltiempo").offset().top;
+                    $("html, body").animate({scrollTop: posi});
+                });
+                $("#btnabout").click(function () {
+                    var posi = $("#about").offset().top;
+                    $("html, body").animate({scrollTop: posi});
+                });
+                $("#btnsabermas").click(function () {
+                    var posi = $("#sabermas").offset().top;
+                    $("html, body").animate({scrollTop: posi});
+                });
+                 $("#subir").click(function () {
+                    $('body,html').animate({scrollTop: 0}, 500);
+                });
+                $("#btnlogo").click(function () {
+                    $('body,html').animate({scrollTop: 0}, 500);
+                });
+                $("#temp2").mouseenter(function(){
+                    $("#temp").show();
+                });
+                $("#temp2").mouseleave(function(){
+                    $("#temp").hide();
+                });
+    
+            });
