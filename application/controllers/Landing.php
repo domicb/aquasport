9be1bebaf34830ef2f9f74d22126c2ed5016e4d6
domicb <domi1213@hotmail.com>
2016-12-08 19:32:44 +0100

    ahora si

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -16,6 +16,16 @@ class landing extends CI_Controller {
         $salida = $this->Articulos_model->tiempo($lugar);
         echo json_encode($salida);
     }
+<<<<<<< HEAD
+=======
+    
+    public function costas()
+    {
+        $lugar = $_POST['sitio'];       
+        $salida = $this->Articulos_model->costas($lugar);
+        echo json_encode($salida);
+    }
+>>>>>>> parent of a94a497... comir
 
     public function aemet() {
 
