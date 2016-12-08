aadb9bf59e5c069f4309f95df0d191f5dce995dc
domicb <domi1213@hotmail.com>
2016-12-08 19:40:38 +0100

    creamos la funcion para recoger la grafica

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -24,7 +24,9 @@ class landing extends CI_Controller {
         $salida = $this->Articulos_model->costas($lugar);
         echo json_encode($salida);
     }
-
+/**
+ * funcion que nos devuelve la prediccion en texto
+ */
     public function aemet() {
 
         $curl = curl_init();
@@ -53,12 +55,39 @@ class landing extends CI_Controller {
         } else {
             echo $response;
         }
+    }
+    /**
+     * funcion que nos devuelve la prediccion en grafica
+     */
+    public function aemet2() {
 
-        /* $response = curl_exec($ch);
-          $json = json_decode($response, true);
-          curl_close($ch);
-          print_r($json);
-          $userEmail = $json["email"]; */
+        $curl = curl_init();
+        header('Content-Type: image/png');
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://opendata.aemet.es/opendata/api/prediccion/maritima/costera/costa/42?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcXVhc3BvcnQuaW5mb3JtQGdtYWlsLmNvbSIsImp0aSI6ImEyNmEwM2ZkLTc3YmUtNGFkMS1iYThkLThjYTIzODRlMjE1MyIsImV4cCI6MTQ4Nzg0OTU0NCwiaXNzIjoiQUVNRVQiLCJpYXQiOjE0ODAwNzM1NDQsInVzZXJJZCI6ImEyNmEwM2ZkLTc3YmUtNGFkMS1iYThkLThjYTIzODRlMjE1MyIsInJvbGUiOiIifQ.HummA5axxrVmaqJJc-0NGDQlCZFI69J-8FBlDP1dftE",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_SSL_VERIFYPEER => "false",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+
     }
 
 }
